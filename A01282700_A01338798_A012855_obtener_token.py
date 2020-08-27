eaff444651f46e0532b9eb365033938cb4305d65
Chito-XD <chito99salu@outlook.com>
2020-08-27 16:17:11 -0500

    update obtener token as the last HW

--- A01282700_A01338798_A012855_obtener_token.py
+++ A01282700_A01338798_A012855_obtener_token.py
@@ -18,18 +18,28 @@ OPB = 102  # Operador binario
 LRP = 103  # Delimitador: paréntesis izquierdo
 RRP = 104  # Delimitador: paréntesis derecho
 END = 105  # Fin de la entrada
+OPC = 106  # Operador condicional
+OPA = 107  # Operador de asignación
+OPR = 108  # Operador relacional
+IDE = 109  # Identificador
 ERR = 200  # Error léxico: palabra desconocida
 
 # Matriz de transiciones: codificación del AFD
 # [renglón, columna] = [estado no final, transición]
 # Estados > 99 son finales (ACEPTORES)
 # Caso especial: Estado 200 = ERROR
-#      dig   op   (    )  raro  esp  .   $
-MT = [[  1, OPB, LRP, RRP,   4,   0, 4, END], # edo 0 - estado inicial
-      [  1, INT, INT, INT, INT, INT, 2, INT], # edo 1 - dígitos enteros
-      [  3, ERR, ERR, ERR,   4, ERR, 4, ERR], # edo 2 - primer decimal flotante
-      [  3, FLT, FLT, FLT, FLT, FLT, 4, FLT], # edo 3 - decimales restantes flotante
-      [ERR, ERR, ERR, ERR,   4, ERR, 4, ERR]] # edo 4 - estado de error
+#      dig   op   (    )  raro  esp  .   $    ?:    =    <    >    !   ID
+MT = [[  1, OPB, LRP, RRP,   4,   0, 4, END, OPC,   5,   7,   7,   10,  9], # edo 0 - estado inicial
+      [  1, INT, INT, INT, INT, INT, 2, INT, INT, INT, INT, INT, INT, INT], # edo 1 - dígitos enteros
+      [  3, ERR, ERR, ERR,   4, ERR, 4, ERR, ERR, ERR, ERR, ERR, ERR, ERR], # edo 2 - primer decimal flotante
+      [  3, FLT, FLT, FLT, FLT, FLT, 4, FLT, FLT, FLT, FLT, FLT, FLT, FLT], # edo 3 - decimales restantes flotante
+      [ERR, ERR, ERR, ERR,   4, ERR, 4, ERR, ERR, ERR, ERR, ERR, ERR, ERR], # edo 4 - estado de error
+      [OPA, ERR, OPA, ERR, ERR, OPA, 4, ERR, ERR,   6, ERR, ERR, ERR, OPA], # edo 5 - primer =
+      [OPR, ERR, OPR, ERR,   4, OPR, 4, OPR, ERR, ERR, ERR, ERR, ERR, OPR], # edo 6 - segundo =
+      [OPR, ERR, OPR, ERR,   4, OPR, 4, OPR, ERR,   8, ERR, ERR, ERR, OPR], # edo 7 - primero operador operacional
+      [OPR, ERR, OPR, ERR,   4, OPR, 4, OPR, ERR, ERR, ERR, ERR, ERR, OPR], # edo 8 - operador relacional
+      [IDE, IDE, IDE, IDE,   4, IDE, 4, IDE, IDE, IDE, IDE, IDE, IDE,   9], # edo 9 - identificador
+      [ERR, ERR, ERR, ERR,   4, ERR, 4, ERR, ERR, OPR, ERR, ERR, ERR, ERR]] # edo 10 - !=
 
 # Filtro de caracteres: regresa el número de columna de la matriz de transiciones
 # de acuerdo al caracter dado
@@ -52,6 +62,18 @@ def filtro(c):
         return 6
     elif c == '$': # fin de entrada
         return 7
+    elif c == '?' or c == ':': # operadores condicionales
+        return 8
+    elif c == '=': # operador relacional
+        return 9
+    elif c == '<': # operador relacional
+        return 10
+    elif c == '>': # operador relacional
+        return 11
+    elif c == '!': # operador relacional
+        return 12
+    elif (ord(c) >= 65 and ord(c) <= 90) or (ord(c) >= 97 and ord(c) <= 122): # identificadores
+        return 13
     else: # caracter raro
         return 4
 
@@ -93,8 +115,24 @@ def obten_token():
         elif edo == END:
             print("Fin de expresion")
             return END
+        elif edo == OPC:
+            lexema += _c 
+            print("Operador condicional ", lexema)
+            return OPC
+        elif edo == OPA:
+            _leer = False
+            print("Operador asignación ", lexema)
+            return OPA
+        elif edo == OPR:
+            lexema += _c 
+            print("Operador relacional ", lexema)
+            return OPR
+        elif edo == IDE:
+            _leer = False
+            print("Identificador ", lexema)
+            return IDE
         else:   
-            leer = False # el último caracter no es raro
+            _leer = False # el último caracter no es raro
             print("ERROR! palabra ilegal", lexema)
             return ERR
             
